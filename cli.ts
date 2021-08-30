ffb1be09ebc0e73e001cf3c86c08dc00ed5efd2b
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2021-08-30 15:52:45 +0200

    Make sure no override for current env exists before using wildcard

--- cli.ts
+++ cli.ts
@@ -31,7 +31,11 @@ async function main() {
   const { data: variables } = await gitlabApi.get<Variable[]>(`/api/v4/projects/${opts.project}/variables?per_page=1000`)
 
   const newEnvVariables = variables.filter(variable => {
-    if (variable.environment_scope == '*') {
+    const isDefault = variable.environment_scope == '*';
+    const noOverrideExists = !variables.some(otherEnv =>
+      otherEnv.key == variable.key && otherEnv.environment_scope == opts.environment
+    )
+    if (isDefault && noOverrideExists) {
       return true
     }
     return opts.environment === variable.environment_scope
